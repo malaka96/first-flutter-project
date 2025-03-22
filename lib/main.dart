7ded9d753c769436e268506c4e7430e210705fe3
malaka96 <madhubhashana655@gmail.com>
2025-03-22 22:11:36 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:first_app/currency_converter_materialapp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: CurrencyConverter());
+  }
+}
