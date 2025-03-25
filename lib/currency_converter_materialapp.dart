dfe3bf56cc482d7c371ecf99b6471d965dc6384a
malaka96 <madhubhashana655@gmail.com>
2025-03-25 13:21:34 +0530

    Styling of textfield

--- lib/currency_converter_materialapp.dart
+++ lib/currency_converter_materialapp.dart
@@ -5,6 +5,37 @@ class CurrencyConverter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hello World!!!')));
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(width: 2.0, style: BorderStyle.solid),
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Malaka",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                hintText: 'Please enter amount in USD',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
